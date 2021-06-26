598d221bb0d09e1eee14e03e0c528b4d0a21e34d
TheNavy <34339285+TheNavy@users.noreply.github.com>
2021-06-26 19:12:49 +0200

    Update Colors

--- ARDUINO_KNX_WS2812B/WS2812B.ino
+++ ARDUINO_KNX_WS2812B/WS2812B.ino
@@ -36,25 +36,25 @@ uint8_t strip_animation(uint8_t animation, struct Loop &loop) {
 }
 
 uint8_t strip0_loop0_eff1(struct Loop &loop) {
-    // Strip ID: 0 - Effect: Fade - LEDS: 120
-    // Steps: 290 - Delay: 1
+    // Strip ID: 0 - Effect: Fade - LEDS: 300
+    // Steps: 210 - Delay: 1
     // Colors: 2 (255.0.0, 0.0.0)
-    // Options: duration=290, every=1, 
-  // if(millis() - strip_0.effStart < 1 * (strip_0.effStep)) return 0x00;
+    // Options: duration=210, every=1, 
+  if(millis() - strip_0.effStart < 1 * (strip_0.effStep)) return 0x00;
   uint8_t r,g,b;
   double e;
-  e = (strip_0.effStep * 1) / (double)290;
+  e = (strip_0.effStep * 1) / (double)210;
   r = ( e ) * 0 + 255 * ( 1.0 - e );
   g = ( e ) * 0 + 0 * ( 1.0 - e );
   b = ( e ) * 0 + 0 * ( 1.0 - e );
-  for(uint16_t j=0;j<120;j++) {
+  for(uint16_t j=0;j<300;j++) {
     if((j % 1) == 0)
       strip_0.strip.setPixelColor(j, r, g, b);
     else
       strip_0.strip.setPixelColor(j, 0, 0, 0);
   }
   
-  if(strip_0.effStep >= 290) {
+  if(strip_0.effStep >= 210) {
     if (loop.numberOfCycles > 0) {
       if (loop.currentCycle >= loop.numberOfCycles) {
         loop.currentCycle = 0;
@@ -76,25 +76,25 @@ uint8_t strip0_loop0_eff1(struct Loop &loop) {
 }
 
 uint8_t strip0_loop0_eff2(struct Loop &loop) {
-    // Strip ID: 0 - Effect: Fade - LEDS: 120
-    // Steps: 240 - Delay: 1
+    // Strip ID: 0 - Effect: Fade - LEDS: 300
+    // Steps: 210 - Delay: 1
     // Colors: 2 (0.255.0, 255.255.255)
-    // Options: duration=240, every=1, 
-  // if(millis() - strip_0.effStart < 1 * (strip_0.effStep)) return 0x00;
+    // Options: duration=210, every=1, 
+  if(millis() - strip_0.effStart < 1 * (strip_0.effStep)) return 0x00;
   uint8_t r,g,b;
   double e;
-  e = (strip_0.effStep * 1) / (double)240;
+  e = (strip_0.effStep * 1) / (double)210;
   r = ( e ) * 255 + 0 * ( 1.0 - e );
   g = ( e ) * 255 + 255 * ( 1.0 - e );
   b = ( e ) * 255 + 0 * ( 1.0 - e );
-  for(uint16_t j=0;j<120;j++) {
+  for(uint16_t j=0;j<300;j++) {
     if((j % 1) == 0)
       strip_0.strip.setPixelColor(j, r, g, b);
     else
       strip_0.strip.setPixelColor(j, 0, 0, 0);
   }
 
-  if(strip_0.effStep >= 240) {
+  if(strip_0.effStep >= 210) {
     if (loop.numberOfCycles > 0) {
       if (loop.currentCycle >= loop.numberOfCycles) {
         loop.currentCycle = 0;
@@ -116,28 +116,28 @@ uint8_t strip0_loop0_eff2(struct Loop &loop) {
 }
 
 uint8_t strip0_loop0_eff3(struct Loop &loop) {
-    // Strip ID: 0 - Effect: Rainbow - LEDS: 120
-    // Steps: 170 - Delay: 20
+    // Strip ID: 0 - Effect: Rainbow - LEDS: 300
+    // Steps: 400 - Delay: 1
     // Colors: 2 (255.0.0, 0.255.0)
-    // Options: rainbowlen=120, toLeft=true, 
-  // if(millis() - strip_0.effStart < 20 * (strip_0.effStep)) return 0x00;
+    // Options: rainbowlen=300, toLeft=true, 
+  if(millis() - strip_0.effStart < 1 * (strip_0.effStep)) return 0x00;
   float factor1, factor2;
   uint16_t ind;
-  for(uint16_t j=0;j<120;j++) {
-    ind = strip_0.effStep + j * 1.4166666666666667;
-    switch((int)((ind % 170) / 85)) {
-      case 0: factor1 = 1.0 - ((float)(ind % 170 - 0 * 85) / 85);
-              factor2 = (float)((int)(ind - 0) % 170) / 85;
+  for(uint16_t j=0;j<300;j++) {
+    ind = strip_0.effStep + j * 1.3333333333333333;
+    switch((int)((ind % 400) / 200)) {
+      case 0: factor1 = 1.0 - ((float)(ind % 400 - 0 * 200) / 200);
+              factor2 = (float)((int)(ind - 0) % 400) / 200;
               strip_0.strip.setPixelColor(j, 255 * factor1 + 0 * factor2, 0 * factor1 + 255 * factor2, 0 * factor1 + 0 * factor2);
               break;
-      case 1: factor1 = 1.0 - ((float)(ind % 170 - 1 * 85) / 85);
-              factor2 = (float)((int)(ind - 85) % 170) / 85;
+      case 1: factor1 = 1.0 - ((float)(ind % 400 - 1 * 200) / 200);
+              factor2 = (float)((int)(ind - 200) % 400) / 200;
               strip_0.strip.setPixelColor(j, 0 * factor1 + 255 * factor2, 255 * factor1 + 0 * factor2, 0 * factor1 + 0 * factor2);
               break;
     }
   }
 
-  if(strip_0.effStep >= 170) {
+  if(strip_0.effStep >= 400) {
     if (loop.numberOfCycles > 0) {
       if (loop.currentCycle >= loop.numberOfCycles) {
         loop.currentCycle = 0;
@@ -160,7 +160,7 @@ uint8_t strip0_loop0_eff3(struct Loop &loop) {
 
 void strip0_loop0_reset() {
 	
-  // Set black - necessary?
+  // Set black
 	strip_0.strip.clear();
 
   // Reset strip values
